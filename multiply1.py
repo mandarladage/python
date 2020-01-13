db0afd05e47cba3597cbfd6b231371c5850a651d
mandarladage <57866586+mandarladage@users.noreply.github.com>
2020-01-13 19:46:37 +0530

    Add files via upload
    basic code for multiplying two integers.

---
+++ multiply1.py
@@ -0,0 +1,5 @@
+print ("Multiplication of two integers")
+x = int(input("please enter the first number: "))
+y = int(input("please enter the second number: "))
+z = x*y
+print ("multiplication is : ",z)
