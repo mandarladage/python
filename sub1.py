cd4e1115d8161d77b9526180a86cc63c5f442e4b
mandarladage <57866586+mandarladage@users.noreply.github.com>
2020-01-13 19:40:25 +0530

    Add files via upload
    basic code for substraction of two integers.

---
+++ sub1.py
@@ -0,0 +1,5 @@
+print("Substraction of two integers!!!\n")
+x = int(input("Please enter the first number: "))
+y = int(input("please enter the second number: "))
+z = x-y
+print("Substraction is: ",z)
